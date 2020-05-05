75c97cf04b0baf1ddcfbc8dfd6c2be7a7c2e236c
TrungKien <trungkien2000.tx@gmail.com>
2020-05-05 15:29:26 +0700

    update 2

--- script.js
+++ script.js
@@ -60,14 +60,43 @@ test4ChangeColor = function() {
     document.getElementById('test4Text').style.color = randomColor;
 }
 
+var checkTest5 = false;
 test5Start = function() {
-    var count = 0;
-    setInterval(function() {
-        count++;
-        document.getElementById('test5Output').value = count;
+    var count5 = 0;
+    var sI5 = setInterval(function() {
+        if (checkTest5) {
+            clearInterval(sI5);
+            count5 = 0;
+        }
+        count5++;
+        document.getElementById('test5Output').innerHTML = count5;
     }, 1000);
+    return sI5;
 }
 
 test5Stop = function() {
+    checkTest5 = true;
+    document.getElementById('test5Output').innerHTML = "1";
+}
+
+test6Play = function() {
+    var count6 = 0;
+    const temp = 'test6-'
+    for (let i = 1; i <= 6; i++) {
+        setTimeout(function() {
+
+        }, 1000)
+    }
+    var sI6 = setInterval(function() {
+        count6++;
+        var show = temp + count6;
+        console.log(count6);
+        console.log(show);
+        document.getElementById('test6Output').innerHTML = document.getElementById('show');
+    }, 1000);
+    return sI6;
+}
+
+test6Stop = function() {
 
 }
